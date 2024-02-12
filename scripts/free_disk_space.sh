99a30689baa2337cf76b533292cb708f72b7e2c0
iousc <iousc26@gmail.com>
2024-02-11 20:43:52 -0500

    update docker-publish workflow and free_disk_space script

--- scripts/free_disk_space.sh
+++ scripts/free_disk_space.sh
@@ -33,15 +33,22 @@ echo "Listing 100 largest packages"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 df -h
 echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
 sudo apt-get remove -y '^dotnet-.*'
 sudo apt-get remove -y '^llvm-.*'
 sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
 sudo apt-get autoremove -y
 sudo apt-get clean
 df -h
 echo "Removing large directories"
-rm -rf /usr/share/dotnet/
-rm -rf /opt/hostedtoolcache
+sudo rm -rf /usr/share/dotnet/
+sudo rm -rf /usr/local/graalvm/
+sudo rm -rf /usr/local/.ghcup/
+sudo rm -rf /usr/local/share/powershell
+sudo rm -rf /usr/local/share/chromium
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/local/lib/node_modules
+sudo rm -rf /opt/hostedtoolcache
 df -h
